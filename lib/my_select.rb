011aaf75e229bdb49e273a45bffdd927f82940cb
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-10 15:37:56 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
+require "pry"
 def my_select(collection)
- # your code here!
+    i = 0
+    new_array = []
+  while i < collection.length
+  yield(collection[i])
+    if i == true
+    new_array << collection.values_at(i)
+  end
+    i += 1
+  end
+  new_array
 end
